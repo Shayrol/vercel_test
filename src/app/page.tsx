42c7dd5c5dbbcc989af748d82108efe49ccabd82
Shayrol <rus47@naver.com>
2025-06-03 21:23:26 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Vercel Test App</h1>
+      <p>This is a simple page to test Vercel deployment.</p>
+    </div>
+  );
+}
